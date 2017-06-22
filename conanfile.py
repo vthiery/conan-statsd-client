39878e8148a5491ac3f15a0a0113356f6d56722f
Vincent Thiery <vjmthiery@gmail.com>
2017-06-22 20:31:53 +0200

    Add description and remove path option

--- conanfile.py
+++ conanfile.py
@@ -3,19 +3,17 @@ from conans import ConanFile
 class StatsdClient(ConanFile):
     name = "statsdclient"
     version = "0.0.2"
+    description = "Statsd Client from https://github.com/vthiery/cpp-statsd-client"
     license = "MIT"
     url = "https://github.com/vthiery/conan-statsd-client"
     author = "Vincent Thiery (vjmthiery@gmail.com)"
-    settings = None
-    options = {"path": "ANY"}
-    default_options = "path="
 
     def source(self):
         self.run("git clone --recursive https://github.com/vthiery/cpp-statsd-client")
 
     def package(self):
-        header_dir = "include"
-        if self.options.path != "":
-            header_dir += "/" + str(self.options.path)
-        self.copy("*.hpp", dst=header_dir, src="cpp-statsd-client/src", keep_path=True)
+        self.copy("*.hpp", dst="include")
 
+    def package_info(self):
+        self.cpp_info.libdirs = []
+        self.cpp_info.bindirs = []
